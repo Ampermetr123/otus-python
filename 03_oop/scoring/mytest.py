43d269a677efcd41313de9e5f74e7438a82b4eac
Sergey Simonov <sb.simonov@gmail.com>
2019-02-06 17:55:49 +0300

    обновления теста для обсуждения

--- 03_oop/scoring/mytest.py
+++ 03_oop/scoring/mytest.py
@@ -8,9 +8,20 @@ class MyRequestB(api.Request):
     first_name = api.CharField(required=False, nullable=True)
     last_name = api.CharField(required=False, nullable=True)
 
+class SomeClass():
+    field_1= api.CharField(required=False, nullable=True)
+    field_2= api.CharField(required=True, nullable=True)
+
 if __name__ == '__main__':
 
     reqA_inst = MyRequestA({'first_name':"A_first", "last_name":"A_last"})
     reqB_inst = MyRequestB({'first_name':"B_first", "last_name":"B_last"})
-
     print (reqA_inst.first_name, reqA_inst.last_name, reqB_inst.first_name, reqB_inst.last_name)
+
+    try:
+        someclass_inst = SomeClass()
+        print(someclass_inst.field_1) 
+        print(someclass_inst.field_2) 
+    except api.ValidationError as err:
+        print(str(err))
+      
